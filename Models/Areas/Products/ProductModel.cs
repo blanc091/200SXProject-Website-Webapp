6a55597e9fe2c4a0b3eef792a43bf07d82d7992d
blanc091 <mircea.albu91@gmail.com>
2025-02-21 01:14:37 +0200

    Fixed add product price length validation.

--- Models/Areas/Products/ProductModel.cs
+++ Models/Areas/Products/ProductModel.cs
@@ -1,14 +1,18 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace _200SXContact.Models.Areas.Products
 {
     public class Product
     {
         public int Id { get; set; }
-        public required string Name { get; set; }
-        public required string Category { get; set; }
-        public required string Description { get; set; }
-        [Column(TypeName = "decimal(4,2)")]
+		[Required(ErrorMessage = "Product Name is required.")]
+		public string Name { get; set; }
+		[Required(ErrorMessage = "Category is required.")]
+		public string Category { get; set; }
+		[Required(ErrorMessage = "Description is required.")]
+		public string Description { get; set; }
+		[Column(TypeName = "decimal(6,2)"), Range(0, 9999.99, ErrorMessage = "Price must be between 0 and 9999.99.")]
         public required decimal Price { get; set; }
         public List<string> ImagePaths { get; set; } = new List<string>();
         public DateTime DateAdded { get; set; }
