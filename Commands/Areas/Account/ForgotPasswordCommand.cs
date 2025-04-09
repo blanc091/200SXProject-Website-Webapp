89c3977ae528a57649e8a00a98bc08cc26447a1b
blanc091 <mircea.albu91@gmail.com>
2025-04-09 23:39:42 +0300

    Forgot Pass Reset initial form.

--- Commands/Areas/Account/ForgotPasswordCommand.cs
+++ Commands/Areas/Account/ForgotPasswordCommand.cs
@@ -18,6 +18,24 @@ namespace _200SXContact.Commands.Areas.Account
         }
         public async Task<ForgotPasswordCommandResult> Handle(ForgotPasswordCommand command, CancellationToken cancellationToken)
         {
+            await _loggerService.LogAsync($"ForgotPassword || Starting sending password reset link to {command.Email}", "Info", "");
+
+            ForgotPasswordCommandValidator validator = new ForgotPasswordCommandValidator();
+            ValidationResult validationResult = await validator.ValidateAsync(command, cancellationToken);
+
+            if (!validationResult.IsValid)
+            {
+                string[] errorMessages = validationResult.Errors.Select(e => e.ErrorMessage).ToArray();
+
+                await _loggerService.LogAsync($"ForgotPassword || Validation error: {string.Join(", ", errorMessages)}", "Error", "");
+
+                return new ForgotPasswordCommandResult
+                {
+                    Succeeded = false,
+                    Errors = errorMessages
+                };
+            }
+
             await _emailService.SendPasswordResetEmail(command.Email, command.ResetUrl);
 
             await _loggerService.LogAsync($"ForgotPassword || Sent reset link to {command.Email}", "Info", "");
@@ -33,5 +51,12 @@ namespace _200SXContact.Commands.Areas.Account
     {
         public bool Succeeded { get; set; }
         public IEnumerable<string>? Errors { get; set; }
+    }
+    public class ForgotPasswordCommandValidator : AbstractValidator<ForgotPasswordCommand>
+    {
+        public ForgotPasswordCommandValidator()
+        {
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email is required !").EmailAddress().WithMessage("Please enter a valid email address !").MaximumLength(50).WithMessage("Email cannot exceed 50 characters !");
+        }
     }
 }
